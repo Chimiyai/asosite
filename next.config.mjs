c01cbbb83ddfd581446ba5adf18d01f05464774a
Emre Bulut <005emreebulutt005@gmail.com>
2025-06-23 17:23:59 +0300

    vercel build hatası giderildi

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,15 @@
-// next.config.mjs
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: '**',
-      },
-    ],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    appDir: true,
-    serverActions: {
-      bodySizeLimit: '2mb',
+   /** @type {import('next').NextConfig} */
+   const nextConfig = {
+    images: {
+      remotePatterns: [
+        { protocol: 'https', hostname: '**' },
+        { protocol: 'http', hostname: '**' },
+      ],
     },
-  }
-};
+    output: 'standalone',
+    typescript: { ignoreBuildErrors: true },
+    eslint: { ignoreDuringBuilds: true },
+  };
+  export default nextConfig;
 
-export default nextConfig;
+export const dynamic = "force-dynamic";
